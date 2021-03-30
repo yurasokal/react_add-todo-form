ae3a018371ae3737b41432d81d8e3fdada8fba1c
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-30 09:21:56 +0300

    Updates

---
+++ precache-manifest.f268c2d13015c38ca4182874b7cae3f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e7a321d97436bda3380e5c0aa5fe3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "74f819a5f7a040b7a76f",
+    "url": "./static/css/2.d1ab60d4.chunk.css"
+  },
+  {
+    "revision": "dd364ae8afc0bf3936e3",
+    "url": "./static/css/main.7bb99c4c.chunk.css"
+  },
+  {
+    "revision": "74f819a5f7a040b7a76f",
+    "url": "./static/js/2.84e96cca.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.84e96cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd364ae8afc0bf3936e3",
+    "url": "./static/js/main.29bd6491.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ffde3675347713e2155b1328e0cfa1a1.js"
+  "./precache-manifest.f268c2d13015c38ca4182874b7cae3f4.js"
 );
 
 self.addEventListener('message', (event) => {
